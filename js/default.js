df7c8e5ef9a08df8ee1117549d8e69a439468425
choijinhyuck0702 <79233908+choijinhyuck0702@users.noreply.github.com>
2022-02-23 15:16:54 +0900

    Add files via upload

--- js/default.js
+++ js/default.js
@@ -5,58 +5,19 @@ $(document).on('mouseleave', '.pa-menu', function(){
     $('.ch-menu').slideUp(200);
 });
 
-let slideWrapper = document.getElementById('slider-wrap')
-let slideIndex = 0;
-let slides = document.querySelectorAll('#slider-wrap ul#slider li');
-let totalSlides = slides.length;
-let sliderWidth = slideWrapper.clientWidth;
-
-slides.forEach(function(element){
-    element.style.width = sliderWidth + 'px';
-});
-
-let slider = document.querySelector('#slider-wrap ul#slider');
-slider.style.width = sliderWidth * totalSlides + 'px';
-
-let nextBtn = document.getElementById('next');
-let prevBtn = document.getElementById('previous');
-nextBtn.addEventListener('click', function(){
-    plusSlides(1);
-});
-prevBtn.addEventListener('click', function(){
-    plusSlides(-1);
-});
-
-slideWrapper.addEventListener('mouseover', function(){
-    this.classList.add('active');
-    clearInterval(autoSlider);
-});
-slideWrapper.addEventListener('mouseleave', function(){
-    this.classList.remove('active');
-    autoSlider = setInterval(function(){
-        plusSlides(1);
-    }, 3000);
-});
-
-function plusSlides(n){
-    slides[slideIndex].style.opacity = '1';
-    showSlides(slideIndex += n);
-    slides[slideIndex].style.opacity = '0.7';
-}
-function currentSlides(n){
-    showSlides(slideIndex = n);
-}
-function showSlides(n){
-    slideIndex = n;
-    if(slideIndex == -1){
-        slideIndex = totalSlides - 1;
-    } else if (slideIndex === totalSlides){
-        slideIndex = 0;
+let slideEls = document.querySelectorAll('.slider .img-box');
+let selectIdx = 1;
+function changeSlide(idx){
+    for(let cnt=0;cnt<slideEls.length;cnt++){
+        slideEls[cnt].style.opacity = '0';
     }
-    slider.style.left = -(sliderWidth * slideIndex) + 'px';
+    slideEls[idx].style.opacity = '1';
 }
-let autoSlider = setInterval(function(){
-    plusSlides(1);
+setInterval(function(){
+    if(selectIdx>2){
+        selectIdx = 0;
+    }
+    changeSlide(selectIdx++);
 }, 3000);
 
 $('.modal1').click(function(){
